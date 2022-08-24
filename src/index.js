960c29cbe4f084388bf4a8d5d5750318cd616c77
Lucas França <102264849+Suubiprabaxo@users.noreply.github.com>
2022-08-24 20:36:51 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "font-awesome/css/font-awesome.min.css";
+import { BrowserRouter as Router } from "react-router-dom";
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
